6f84e76aa71a14cf9c828d067c0e27638b9b5019
Shisha1536 <schischkoff2017@yandex.ru>
2023-09-25 16:36:13 +0300

    js start

---
+++ task_3/index.js
@@ -0,0 +1,3 @@
+const btn_send = document.getElementsByClassName('btn_send');
+const btn_geolocation = document.getElementsByClassName('btn_geolocation');
+let correspondence = document.getElementsByClassName('correspondence');
