06b401a8f0711579be2867e0520e7f2be0814aef
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2023-09-27 22:16:05 +0300

    fin task_3

--- task_3/index.js
+++ task_3/index.js
@@ -1,3 +1,49 @@
-const btn_send = document.getElementsByClassName('btn_send');
-const btn_geolocation = document.getElementsByClassName('btn_geolocation');
-let correspondence = document.getElementsByClassName('correspondence');
+const btnSend = document.getElementById('btn_send');
+const btnGeolocation = document.getElementById('btn_geolocation');
+
+btnSend.addEventListener('click', () => {
+    const url = 'wss://echo-ws-service.herokuapp.com';
+    let chat = document.getElementById('correspondence');
+    let wb = new WebSocket(url);
+    
+    function client(text) {
+        let message = document.createElement('p');
+        chat.appendChild(message);
+        message.textContent = text;
+    }
+    function server(text) {
+        let message = document.createElement('p');
+        chat.appendChild(message);
+        message.classList.add('server');
+        message.textContent = text;
+    }
+    wb.onopen =  function (event) {
+        let text = document.getElementById('message').value;
+        client(text);
+        wb.send(text);
+    }
+    wb.onmessage = function (event) {
+        server(event.data);
+    } 
+});
+
+btnGeolocation.addEventListener('click', () => {
+    let chat = document.getElementById('correspondence');
+    
+    const error = () => {
+        let message = document.createElement('p');
+        chat.appendChild(message);
+        message.classList.add('server');
+        message.textContent = "Ошибка получения геоданных";
+    }
+    let success = (position) => {
+        let message = document.createElement('a');
+        chat.appendChild(message);
+        message.classList.add('server');
+        message.textContent = 'Ссылка';
+        message.href = `https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}`;
+    }
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+});
